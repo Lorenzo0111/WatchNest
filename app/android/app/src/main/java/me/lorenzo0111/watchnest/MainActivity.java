5809b23f044876453652949fe2a70690197da087
Lorenzo <hello@lorenzo0111.me>
2024-12-04 19:05:05 +0100

    Initial commit

---
+++ app/android/app/src/main/java/me/lorenzo0111/watchnest/MainActivity.java
@@ -0,0 +1,5 @@
+package me.lorenzo0111.watchnest;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "me.lorenzo0111.watchnest",
+  appName: "WatchNest",
+  webDir: "dist",
+};
+
+export default config;
